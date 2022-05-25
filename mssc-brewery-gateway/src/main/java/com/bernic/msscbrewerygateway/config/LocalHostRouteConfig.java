fe9ebaece09ecb3873d06dc2a17db6e64c8a657d
Ion Bernic <bernic.ion@gmail.com>
2022-05-25 23:42:30 +0300

    Adding configuration for Spring Cloud Gateway. closes #29

---
+++ mssc-brewery-gateway/src/main/java/com/bernic/msscbrewerygateway/config/LocalHostRouteConfig.java
@@ -0,0 +1,22 @@
+package com.bernic.msscbrewerygateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LocalHostRouteConfig {
+
+    @Bean
+    public RouteLocator localHostRoutes(RouteLocatorBuilder locatorBuilder) {
+        return locatorBuilder.routes()
+                .route("beer-service", r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
+                        .uri("http://localhost:8080"))
+                .route("order-service", r -> r.path("/api/v1/customers/**")
+                        .uri("http://localhost:8081"))
+                .route("inventory-service", r -> r.path("/api/v1/beer/*/inventory")
+                        .uri("http://localhost:8082"))
+                .build();
+    }
+}
