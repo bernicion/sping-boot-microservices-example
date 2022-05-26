59b7892762d6c471cc05316d5c06a602b4f76fdc
Ion Bernic <bernic.ion@gmail.com>
2022-05-26 10:43:44 +0300

    Configure Eureka Server. closes #30

---
+++ mssc-brewery-eureka/src/test/java/com/bernic/msscbreweryeureka/MsscBreweryEurekaTest.java
@@ -0,0 +1,11 @@
+package com.bernic.msscbreweryeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MsscBreweryEurekaTest {
+    @Test
+    public void contextLoads() {
+    }
+}
