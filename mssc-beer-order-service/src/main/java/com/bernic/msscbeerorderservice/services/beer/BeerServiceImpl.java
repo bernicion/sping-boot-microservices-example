555dc76b17257ad87f5c40a7a82861d29efff8b5
Ion Bernic <bernic.ion@gmail.com>
2022-06-03 00:41:36 +0300

    Configured fabric8 plugin for docker image creation. closes #41

--- mssc-beer-order-service/src/main/java/com/bernic/msscbeerorderservice/services/beer/BeerServiceImpl.java
+++ mssc-beer-order-service/src/main/java/com/bernic/msscbeerorderservice/services/beer/BeerServiceImpl.java
@@ -1,6 +1,7 @@
 package com.bernic.msscbeerorderservice.services.beer;
 
 import com.bernic.brewery.model.BeerDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
