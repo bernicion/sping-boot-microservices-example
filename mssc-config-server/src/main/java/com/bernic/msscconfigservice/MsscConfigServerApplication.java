92bf044e5e8d57f1bf8c044b9a3dbcfc85c0056f
Ion Bernic <bernic.ion@gmail.com>
2022-06-02 13:17:38 +0300

    Secured Eureka server. closes #39

--- mssc-config-server/src/main/java/com/bernic/msscconfigservice/MsscConfigServerApplication.java
+++ mssc-config-server/src/main/java/com/bernic/msscconfigservice/MsscConfigServerApplication.java
@@ -3,8 +3,10 @@ package com.bernic.msscconfigservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class MsscConfigServerApplication {
     public static void main(String[] args) {
